20f20a33bd9255ea05f6cfa4136d334d1c3874d9
OrionBT <bradleytimm5@gmail.com>
2023-01-06 12:02:06 -0700

    first commit

---
+++ piLEDblink.py
@@ -0,0 +1,28 @@
+redLed = 21
+greenLed = 20
+blueLed = 26
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BOARD)
+GPIO.setup(redLed, GPIO.OUT)
+GPIO.setup(greenLed, GPIO.OUT)
+GPIO.setup(blueLed, GPIO.OUT)
+while True:
+    GPIO.output(redLed, True) 
+    #GPIO.output(redLed, GPIO.HIGH)
+    time.sleep(.1)
+    GPIO.output(redLed, False)
+    #GPIO.output(redLed, GPIO.LOW)
+    GPIO.output(greenLed, True) 
+    #GPIO.output(redLed, GPIO.HIGH)
+    time.sleep(.1)
+    GPIO.output(greenLed, False)
+    #GPIO.output(redLed, GPIO.LOW)
+    GPIO.output(blueLed, True) 
+    #GPIO.output(redLed, GPIO.HIGH)
+    time.sleep(.1)
+    GPIO.output(blueLed, False)
+    #GPIO.output(redLed, GPIO.LOW)
+    time.sleep(1)
+    
